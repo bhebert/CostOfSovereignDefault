5fa5e6f66193af683b0a2d99b89d29efe0defa84
jschreger <jschreger@hbs.edu>
2016-04-19 17:22:13 -0400

    Add Bloomberg and Datastream

--- ProbabilityOfDefault/DefProb_Bootstrap.m
+++ ProbabilityOfDefault/DefProb_Bootstrap.m
@@ -15,6 +15,8 @@ assignin('base','csvfile_europe',[apath 'Matlab_Europe_zero.csv'])
 assignin('base','csvfile_newyork',[apath 'Matlab_NewYork_zero.csv'])
 assignin('base','csvfile_ust_europe',[apath 'Matlab_Europe_spreads_zero_UST.csv'])
 assignin('base','csvfile_ust_newyork',[apath 'Matlab_NewYork_spreads_zero_UST.csv'])
+assignin('base','csvfile_bb',[apath 'Matlab_BBspreads_zero_UST.csv'])
+assignin('base','csvfile_ds',[apath 'Matlab_DSspreads_zero_UST.csv'])
 
 
 %COMPOSITE
@@ -203,5 +205,82 @@ end
 keymat=[date,ProbDef_mat,Haz_mat,time_est];
 cd(apath)
 csvwrite('Bootstrap_June16.csv',keymat)
+
+
+
+%%%%%%%%%%%%%%%%
+%BLOOMBERG and Datastream%
+%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%USING US Treasury Zeros to discount.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+test=0;
+for i=1:2
+    if i==1
+       dataset=csvread(csvfile_bb);
+    elseif i==2
+        dataset=csvread(csvfile_ds);
+    elseif i==3
+        %dataset=csvread(csvfile_ust_newyork);    
+    end
+    
+if test==1 
+    dataset=dataset(700:710,:);
+end
+date=dataset(:,1);
+recovery=dataset(:,2)/100;
+parspreads=dataset(:,3:7);
+irs=dataset(:,8:12);
+date=date+715876; %convert Stata date to Matlab
+date_id=datestr(date);
+irs_length=linspace(1,5,5); %maturities of interest rate swaps
+spread_length=[1,2,3,4,5]; %maturities of CDS
+days_to_add=round(365.*irs_length);
+recoveryConH=ones(size(recovery))*.395;
+
+%Run code in parallel to spped up
+parfor i=1:length(date)
+    Settle=date(i);
+    Spread_Time=spread_length;
+    Spread=100*parspreads(i,:);
+    Market_Dates=daysadd(date(i),round(365.*spread_length)); %maturity dates of all CDS
+    MarketData=[Market_Dates, Spread'];
+    Zero_Time=[spread_length]';
+    Zero_Rate=irs(i,:)'/100;
+    Zero_Dates=daysadd(date(i),round(365.*irs_length)); %maturity dates of discount curve
+    ZeroData=[Zero_Dates Zero_Rate];
+    [ProbData,HazData] = cdsbootstrap(ZeroData,MarketData,Settle,'RecoveryRate',recovery(i));
+    %assume constant hazard rate of 39.5%
+    [ProbDataConH,HazDataConH] = cdsbootstrap(ZeroData,MarketData,Settle,'RecoveryRate',recoveryConH(i));
+    ProbDef_mat(i,:)=ProbData(:,2)';
+    Haz_mat(i,:)=HazData(:,2)';
+    
+    ProbDef_matConH(i,:)=ProbDataConH(:,2)';
+    Haz_matConH(i,:)=HazDataConH(:,2)';
+    if mod(i,10)==0
+        display i
+    end
+end
+
+
+%savefile=[mpath 'Bootstrap_results_UST.mat'];
+%save(savefile,'ProbDef_mat','Haz_mat','date','date_id','dataset')
+%savefile=[mpath 'Bootstrap_results_UST.mat'];
+%save(savefile,'ProbDef_matConH','Haz_matConH','date','date_id','dataset')
+date_stata=date-715876;
+keymat=[date_stata,ProbDef_mat,Haz_mat];
+keymatConH=[date_stata,ProbDef_matConH,Haz_matConH];
+
+cd(apath)
+if i==1
+csvwrite('Bootstrap_results_BBUST.csv',keymat)
+csvwrite('Bootstrap_resultsConH_BBUST.csv',keymatConH)
+elseif i==2
+   csvwrite('Bootstrap_results_DSUST.csv',keymat)
+    csvwrite('Bootstrap_resultsConH_DSUST.csv',keymatConH)
+elseif i==3
+end
+
+end
 
 
